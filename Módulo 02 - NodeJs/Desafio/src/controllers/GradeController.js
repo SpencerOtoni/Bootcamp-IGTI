13057dcfeaed21ad24682f8fccc66918350b511e
Spencer <sspencerotoni@gmail.com>
2020-08-06 22:54:24 -0300

    Commit

--- Módulo 02 - NodeJs/Desafio/src/controllers/GradeController.js
+++ Módulo 02 - NodeJs/Desafio/src/controllers/GradeController.js
@@ -4,6 +4,24 @@ const { writeFile, readFile } = fs;
 const FileNameArchive = "./grades.json";
 
 class GradeController {
+  async store(req, res) {
+    try {
+      let grade = req.body;
+
+      const data = JSON.parse(await readFile(FileNameArchive));
+      let { grades } = data;
+
+      grade = { id: data.nextId++, ...grade, timestamp: new Date() };
+
+      grades.push(grade);
+      await writeFile(FileNameArchive, JSON.stringify(data, null, 2));
+
+      res.send(grade);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async index(req, res) {
     try {
       const data = JSON.parse(await readFile(FileNameArchive));
@@ -16,26 +34,87 @@ class GradeController {
 
   async show(req, res) {
     try {
-      const { id } = req.params
+      const { id } = req.params;
       const data = JSON.parse(await readFile(FileNameArchive));
       const { grades } = data;
 
-      const student = grades.find(student => {
-        return student.id === parseInt(id)
-      })
+      const student = grades.find((student) => {
+        return student.id === parseInt(id);
+      });
 
-      res.send(student);
+      if (student) {
+        res.send(student);
+      } else {
+        res.status(400).send({ error: "Usuário não encontrado." });
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
-  async store(req, res){
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const gradeBody = req.body;
 
+      const data = JSON.parse(await readFile(FileNameArchive));
+      const { grades } = data;
+
+      const indexStudent = grades.findIndex((grade) => {
+        return grade.id === parseInt(id);
+      });
+
+      if (indexStudent !== -1) {
+        const { id, student, subject, type, value, timestamp } = grades[
+          indexStudent
+        ];
+
+        const grade = {
+          id,
+          student: gradeBody.student,
+          subject: gradeBody.subject,
+          type: gradeBody.type,
+          value: gradeBody.value,
+          timestamp,
+        };
+
+        grades[indexStudent] = grade;
+        await writeFile(FileNameArchive, JSON.stringify(data, null, 2));
+
+        res.send(grade);
+      } else {
+        res.status(400).send({ error: "Usuário não cadastrado." });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  async update(req, res){
-    
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const data = JSON.parse(await readFile(FileNameArchive));
+      let { grades } = data;
+
+      const indexStudent = grades.findIndex((grade) => {
+        return grade.id === parseInt(id);
+      });
+
+      if (indexStudent !== -1) {
+        const { student } = grades[indexStudent];
+        data.grades = grades.filter((student) => {
+          return student.id !== parseInt(id);
+        });
+        await writeFile(FileNameArchive, JSON.stringify(data, null, 2));
+
+        res.send({ Msg: `Usuário ${student}, foi excluido com sucesso!` });
+      } else {
+        res.status(400).send({ error: "Usuário não cadastrado." });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
--- Módulo 02 - NodeJs/Desafio/src/routes.js
+++ Módulo 02 - NodeJs/Desafio/src/routes.js
@@ -4,7 +4,10 @@ import GradeController from "./controllers/GradeController.js";
 
 const routes = express.Router();
 
-routes.get("/grade", GradeController.index);
+routes.post("/grade", GradeController.store);
+routes.put("/grade/:id", GradeController.update);
+routes.delete("/grade/:id", GradeController.delete);
 routes.get("/grade/:id", GradeController.show);
+routes.get("/grade", GradeController.index);
 
 export default routes;
