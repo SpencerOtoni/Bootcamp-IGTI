e3805a1749caf7e6ef9d78d7d01189069c6e8fe7
Spencer <sspencerotoni@gmail.com>
2020-08-31 20:55:08 -0300

    commit

---
+++ Módulo 04 -MongoDB/Trabalho Prático/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
+import connection from "./database/index.js";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares()
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
--- Módulo 04 -MongoDB/Trabalho Prático/src/server.js
+++ Módulo 04 -MongoDB/Trabalho Prático/src/server.js
@@ -1,13 +1,4 @@
-import express from "express";
-import cors from "cors";
-import routes from "./routes.js";
-import connection from "./database/index.js";
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(routes);
+import app from './app.js'
 
 app.listen(3000, () => {
   console.log("API em execucao");
